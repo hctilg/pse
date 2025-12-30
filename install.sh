5945758df5cfca8e4baaf775e121b8ed1ef692e3
Mahoor Nemati ✨ <98208009+hctilg@users.noreply.github.com>
2025-12-30 21:35:45 +0330

    Update install script for better variable usage

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@ set -e
 
 APP_NAME="pse"
 SCRIPT_URL="https://raw.githubusercontent.com/hctilg/pse/main/strength.sh"
+CPS_URL="https://raw.githubusercontent.com/hctilg/pse/main/common_passwords.txt"
 
 if [[ $(uname -o) == "Android" ]]; then # Termux
   INSTALL_PATH="/data/data/com.termux/files/usr/bin/$APP_NAME"
 
-  echo -e "\n  [#] Installing sst..."
+  echo -e "\n  [#] Installing $APP_NAME..."
   curl -s -o "$INSTALL_PATH" "$SCRIPT_URL"
 
   echo -e "\n  [#] Set executable permissions..."
@@ -15,12 +16,15 @@ if [[ $(uname -o) == "Android" ]]; then # Termux
 else
   INSTALL_PATH="/usr/local/bin/$APP_NAME"
 
-  echo -e "\n  [#] Installing sst..."
+  echo -e "\n  [#] Installing $APP_NAME..."
   sudo curl -s -o "$INSTALL_PATH" "$SCRIPT_URL"
 
   echo -e "\n  [#] Set executable permissions..."
   sudo chmod +x "$INSTALL_PATH"
 fi
+
+echo -e "\n  [#] Downloading `common_passwords.txt`..."
+curl -s -o "~/common_passwords.txt" "$CPS_URL"
 
 echo -e "\n  [#] Installation completed !"
 echo -e "\n - You can now run the script with the command '$APP_NAME'.\n"
