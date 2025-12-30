e4901fe9b2f11133918d32dc88832084c133284c
Mahoor Nemati ✨ <98208009+hctilg@users.noreply.github.com>
2025-12-30 21:50:38 +0330

    Fix path for downloading common passwords file

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ else
 fi
 
 echo -e "\n  [#] Downloading common passwords..."
-curl -s -o "~/common_passwords.txt" "$CPS_URL" 
+curl -s -o "$HOME/common_passwords.txt" "$CPS_URL" 
 
 echo -e "\n  [#] Installation completed !"
 echo -e "\n - You can now run the script with the command '$APP_NAME'.\n"
